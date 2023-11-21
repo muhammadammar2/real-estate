e9a864fd18840f43375cdf6ad2e640b56e47f60b
muhammadammar2 <uExistentialist@proton.me>
2023-11-21 03:02:35 -0800

    sleepy

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,6 +1,7 @@
 import Listing from "../models/lisitng.model.js";
 import { errorHandler } from "../utils/error.js";
 
+//create
 export const createListing = async (req, res, next) => {
   try {
     const listing = await Listing.create(req.body);
@@ -10,6 +11,7 @@ export const createListing = async (req, res, next) => {
   }
 };
 
+//delete
 export const deleteListing = async (req, res, next) => {
   const listing = await Listing.findById(req.params.id);
   if (!listing) {
@@ -21,6 +23,40 @@ export const deleteListing = async (req, res, next) => {
   try {
     await Listing.findByIdAndDelete(req.params.id);
     res.status(200).json("Listing has been deleted");
+  } catch (error) {
+    next(error);
+  }
+};
+
+//update
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found"));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can only update your own listings"));
+  }
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedListing);
+  } catch (error) {
+    next(error);
+  }
+};
+
+//get/:id (by that we need to use params to find id)
+export const getListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return next(errorHandler(401, "Listing not found"));
+    }
+    res.status(200).json(listing);
   } catch (error) {
     next(error);
   }
