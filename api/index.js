14a88d049282059a9933931d45522da9e291a2d5
muhammadammar2 <uExistentialist@proton.me>
2023-11-26 11:43:35 -0800

    final

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ mongoose
   });
 
 //deploying shit
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 const app = express();
 
@@ -37,11 +37,11 @@ app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
 //deploying shit
-app.use(express.static(path.join(__dirname, "/client/dist")));
+// app.use(express.static(path.join(__dirname, "/client/dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+// });
 
 //error middleware
 //err is the middleware & next traverses to next middleware
