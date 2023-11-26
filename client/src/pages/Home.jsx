1736e13177273d263ab7ef228258d6f22bd7b6b4
muhammadammar2 <uExistentialist@proton.me>
2023-11-25 23:58:16 -0800

    about

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -50,15 +50,16 @@ export default function Home() {
       {/* top */}
       <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
         <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
-          Find your next <span className="text-slate-500">perfect</span>
+          Discover your ideal <span className="text-slate-500">home</span>
           <br />
-          place with ease
+          effortlessly.
         </h1>
         <div className="text-gray-400 text-xs sm:text-sm">
-          Sahand Estate is the best place to find your next perfect place to
-          live.
+          Metro Estate offers the finest selection of residences for your next
+          ideal living space.
           <br />
-          We have a wide range of properties for you to choose from.
+          Explore our diverse range of properties and find the perfect home for
+          you.
         </div>
         <Link
           to={"/search"}
