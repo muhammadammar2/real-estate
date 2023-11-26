4bd89dae332e188bf826a16edc940518cc7544df
muhammadammar2 <uExistentialist@proton.me>
2023-11-26 01:10:26 -0800

    deploying

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import listingRouter from "./routes/listing.route.js";
+import path from "path";
 dotenv.config();
 
 //connection of db
@@ -16,6 +17,9 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+//deploying shit
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -31,6 +35,13 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+//deploying shit
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 //error middleware
 //err is the middleware & next traverses to next middleware
